dfcddcb9523657d6ed1233fbf28c50e7dacabbf6
flucas97 <feijolucas1997@gmail.com>
2020-06-21 00:44:15 -0300

    100% coverage

--- src/domain/access_token/access_token_test.go
+++ src/domain/access_token/access_token_test.go
@@ -2,11 +2,33 @@ package access_token
 
 import (
 	"testing"
+	"time"
 )
 
 func TestGetNewAccessToken(t *testing.T) {
 	at := &AccessToken{}
 	if at.IsExpired() {
 		t.Error("new access token should now be expired")
+	}
+
+	if at.AccessToken != "" {
+		t.Error("new access token should not have defined access token id")
+	}
+
+	if at.UserID != 0 {
+		t.Error("new access token should not have an associated user id")
+	}
+}
+
+func TestIsExpired(t *testing.T) {
+	at := &AccessToken{}
+
+	if at.IsExpired() {
+		t.Error("new access token should not be expired by default")
+	}
+
+	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
+	if at.IsExpired() {
+		t.Error("access token created should not be expired after adding 3 hours")
 	}
 }
