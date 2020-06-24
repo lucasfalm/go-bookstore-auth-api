8a159625405fa132ca5a353a9618ae6bde853b9e
flucas97 <feijolucas1997@gmail.com>
2020-06-24 19:06:50 -0300

    implement GetById db repository method

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,9 +1,15 @@
 package db
 
 import (
+	"fmt"
+
 	"github.com/flucas97/bookstore/auth-api/clients/cassandra"
 	"github.com/flucas97/bookstore/auth-api/src/domain/access_token"
 	"github.com/flucas97/bookstore/auth-api/src/utils/errors_utils"
+)
+
+const (
+	queryGetAccessToken = "SELECT access_token, user_id, client_id, expires FROM access_tokens WHERE access_token=?;"
 )
 
 // entrypoint to use the repository
@@ -18,11 +24,21 @@ func NewRepository() DbRepositoryInterface {
 type dbRepository struct{}
 
 func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors_utils.RestErr) {
+	accessToken := access_token.AccessToken{}
 	session, err := cassandra.GetSession()
 	if err != nil {
-		panic(err)
+		return nil, errors_utils.NewInternalServerError(fmt.Sprintf("error: %v", err.Error()))
 	}
 	defer session.Close()
 
-	return nil, nil
+	if err := session.Query(queryGetAccessToken, id).Scan(
+		&accessToken.AccessToken,
+		&accessToken.UserID,
+		&accessToken.ClientID,
+		&accessToken.Expires,
+	); err != nil {
+		return nil, errors_utils.NewInternalServerError(fmt.Sprintf("error: %v", err.Error()))
+	}
+
+	return &accessToken, nil
 }
